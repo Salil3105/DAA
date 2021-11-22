a23eb0c848b2707f5da0e58ea0af86cea533121d
Salil Chandwadkar <schandwadkar31@gmail.com>
2021-11-22 09:45:35 +0530

    updated

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,8 +1,6 @@
-/* Implementation of QuickSort */
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-// A utility function to swap two elements
 void swap(int *a, int *b)
 {
     int t = *a;
@@ -10,22 +8,15 @@ void swap(int *a, int *b)
     *b = t;
 }
 
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-array, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
 int partition(int arr[], int low, int high)
 {
-    int pivot = arr[high]; // pivot
-    int i = (low - 1);     // Index of smaller element and indicates the right position of pivot found so far
-
+    int pivot = arr[high];
+    int i = (low - 1);
     for (int j = low; j <= high - 1; j++)
     {
-        // If current element is smaller than the pivot
         if (arr[j] < pivot)
         {
-            i++; // increment index of smaller element
+            i++;
             swap(&arr[i], &arr[j]);
         }
     }
@@ -33,44 +24,31 @@ int partition(int arr[], int low, int high)
     return (i + 1);
 }
 
-/* 
-    The main function that implements QuickSort
-    arr[] --> Array to be sorted,
-    low --> Starting index,
-    high --> Ending index 
-*/
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
-        /* pi is partitioning index, arr[p] is now
-		at right place */
         int pi = partition(arr, low, high);
-
-        // Separately sort elements before
-        // partition and after partition
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-/* Function to print an array */
 void printArray(int arr[], int size)
 {
-    int i;
-    for (i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
+    {
         cout << arr[i] << " ";
+    }
     cout << endl;
 }
 
-// Driver Code
 int main()
 {
-    int arr[] = {10, 7, 8, 9, 1, 5};
+    int arr[] = {11, 7, 4, 2, 1, 6};
     int n = sizeof(arr) / sizeof(arr[0]);
-    cout << "Size : " << n << endl;
     quickSort(arr, 0, n - 1);
-    cout << "Sorted array: \n";
+    cout << "Sorted Array : " << endl;
     printArray(arr, n);
     return 0;
 }
